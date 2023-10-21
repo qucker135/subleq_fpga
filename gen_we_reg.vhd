412ee68789ba3c6f38829f23885da7925ed5fc4a
qucker135 <qucker135@gmail.com>
2023-10-21 16:32:44 +0200

    Use linter rules

--- gen_we_reg.vhd
+++ gen_we_reg.vhd
@@ -1,56 +1,61 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    15:21:35 10/15/2023 
--- Design Name: 
--- Module Name:    gen_we_reg - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
+-- Company:
+-- Engineer:
 --
--- Dependencies: 
+-- Create Date:    15:21:35 10/15/2023
+-- Design Name:
+-- Module Name:    gen_we_reg - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool versions:
+-- Description:
 --
--- Revision: 
+-- Dependencies:
+--
+-- Revision:
 -- Revision 0.01 - File Created
--- Additional Comments: 
+-- Additional Comments:
 --
 ----------------------------------------------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+
+library ieee;
+  use ieee.std_logic_1164.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+-- use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+-- library UNISIM;
+-- use UNISIM.VComponents.all;
 
 entity gen_we_reg is
-    generic(
-        width : integer := 8
-    );
-    Port ( clk : in  STD_LOGIC;
-           we : in  STD_LOGIC;
-           din : in  STD_LOGIC_VECTOR ( width-1 downto 0);
-           dout : out  STD_LOGIC_VECTOR ( width-1 downto 0));
-end gen_we_reg;
+  generic (
+    width : integer := 8
+  );
+  port (
+    clk  : in    std_logic;
+    we   : in    std_logic;
+    din  : in    std_logic_vector(width - 1 downto 0);
+    dout : out   std_logic_vector(width - 1 downto 0)
+  );
+end entity gen_we_reg;
 
-architecture Behavioral of gen_we_reg is
+architecture behavioral of gen_we_reg is
 
 begin
 
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            if we = '1' then
-                dout <= din;
-            end if;
-        end if;
-    end process;
-   
-end Behavioral;
+  main : process (clk) is
+  begin
+
+    if rising_edge(clk) then
+      if (we = '1') then
+        dout <= din;
+      end if;
+    end if;
+
+  end process main;
+
+end architecture behavioral;
 
